32b30c1bdfad2cc638366648e702c9fdd73d3a50
Eduardo Dantas Luna <michaelthefear@gmail.com>
2023-05-31 22:36:53 -0300

    Autenticações movidas para arquivo de autenticação

---
+++ DIreto/myapp/auth.py
@@ -0,0 +1,44 @@
+from DIreto.types import Response, Error
+from myapp.models import Usuario as User
+from django.contrib.auth import authenticate, login as django_login
+
+def validate_login(request) -> Response:
+
+    if request.POST.get('username') is None:
+        return (None,"Usuário não foi definido passado")
+    if request.POST.get('password') is None:
+        return (None,"Senha não foi definida passada")
+
+    email = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(request, email = email, password = password)
+    if user is not None:
+        django_login(request,user)
+        return (True,None)
+    
+    return (None,"Usuário ou senha inválidos!")
+
+
+required_fields = ['email','first_name','last_name','password','matricula']
+
+def signin(request) -> Response:
+    POST = request.POST
+    user_att = {}
+    for field in required_fields:
+        post_field = POST.get(field)
+        if post_field is None:
+            return (None,f"{field} não foi definido passado")
+        else:
+            user_att[field] = post_field
+
+    user = User.objects.filter(email=user_att['email']).first()
+    if user is not None:
+        return (None,f"Usuário {user_att['email']} já existe!")
+
+    user = User.objects.create_user(**user_att) # type: ignore
+    user.save()
+
+    return (True,None)
+    
+
--- DIreto/myapp/views.py
+++ DIreto/myapp/views.py
@@ -1,25 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from myapp.models import Usuario as User
-from django.contrib.auth import authenticate, login as login_auth
+from myapp.auth import validate_login, signin
 from django.contrib.auth.decorators import login_required
 
 def login(request):
-
     if request.method == 'GET':
         return render(request, 'login.html')
     else:
-        email = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, email = email, password = password)
-
-        if user is not None:
-            login_auth(request,user)
-
-            return HttpResponse(f"Usuário {email} autenticado com sucesso!")
+        res, error = validate_login(request)
+        if res:
+            return HttpResponse("O usuário autenticado com sucesso!")
         else:
-            return HttpResponse("Usuário ou senha inválidos!")
+            return HttpResponse(error)
         
 
 
@@ -27,25 +20,43 @@ def cadastro(request):
     if request.method == 'GET':
         return render(request, 'cadastro.html')
     else:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        if email is None:
-            return HttpResponse("Email não pode ser nulo!")
-
-        print(email)
-        user = User.objects.filter(email=email).first()
-        if user is not None:
-            return HttpResponse(f"Usuário {username} já existe!")
-        
-        user = User.objects.create_user(username, email, password, informante=True, notificacao_por_email=True, notificacao_por_push=True, matricula="1234567") # type: ignore
-        user.save()
-
-        return HttpResponse(f"Cadastro realizado com sucesso {username}!")
+        res, error = signin(request)
+        if res:
+            return HttpResponse("O usuário foi cadastrado com sucesso!")
+            # Talvez mandar ele para a proxima pagina? 
+        else:
+            return HttpResponse(error)
 
 def index(request):
     return HttpResponse("Home page")
 
+def search(request):
+    return HttpResponse("Busca")
+
+def login_error(request):
+    return HttpResponse("Login inválido")
+
+def cadastro_error(request):
+    return HttpResponse("Cadastro inválido")
+
+def esqueci_senha(request):
+    return HttpResponse("Esqueci minha senha")
+
+@login_required(login_url='/login/')
+def logout(request):
+    return HttpResponse("Logout")
+
 @login_required(login_url='/login/')
 def timeline(request):
     return HttpResponse("Logado")
+
+@login_required(login_url='/login/')
+def configuracoes(request):
+    return HttpResponse("Configurações")
+
+@login_required(login_url='/login/') # fazer com que seja necessario ser staff 
+def novo_post(request):
+    return HttpResponse("Novo post")
+
+def novo_post_error(request):
+    return HttpResponse("Erro ao criar post")
