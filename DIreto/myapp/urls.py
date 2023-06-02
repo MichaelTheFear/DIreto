ad22bf73ac19f9789981960edc569d7f75635a6f
Eduardo Dantas Luna <michaelthefear@gmail.com>
2023-06-02 14:56:22 -0300

    Adicionado rotas e arquivos htmls

--- DIreto/myapp/urls.py
+++ DIreto/myapp/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login'),
-    path('timeline/', views.timeline, name='timeline'),
+    path('search/', views.search, name='search'),
+    path('auth/signin/', views.cadastro, name='cadastro'),
+    path('auth/login/', views.login, name='login'),
+    path('timeline/<str:email>/', views.timeline, name='timeline'),
+    path('config/<str:email>/', views.configuracoes, name='configuracoes'),
+    path('newpost/<str:email>/', views.novo_post, name='novo_post'),
+    path('logout/', views.logout, name='logout'),
+    path('auth/forgotpass/', views.esqueci_senha, name='esqueci_senha'),
 ]
